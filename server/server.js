69c01402c52b1e682b9a91e1ad4c6f113b1cb92f
Neftaly Lugo <20163825@itla.edu.do>
2018-06-09 18:45:56 -0400

    Todo esta trabajando bien, proximo paso url del html

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-import path from 'path';
 import app from './config/express';
+import path from 'path';
 
-// const PORT = process.env || 3825;
+const PORT = process.env.NODE_ENV || 3825;
 
-// app.listen(3825, function(error){
-//     console.log('Server on port 3825');
-// });
 
-app.listen(3825, (error) => {
-    if (error) console.log(error);
-    console.log(`Server on port: 3825`);
+app.all('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/client/public/views/index.html'));
+});
+
+
+
+app.listen(PORT, (error) => {
+    //if (error) console.log(error);
+    console.log(`Server on port: ${PORT}`);
+    // console.log(path.join(__dirname, '..', 'client/public/views/index.html'));
+    // console.log(path.join(__dirname, '..', '/client/public/views/index.html'));
+    // console.log(path.join(__dirname, '../client/public/views/index.html'));
 });
