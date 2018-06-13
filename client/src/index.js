40d95afe546e2231c12d113ccca6e59dff89499d
Neftaly Lugo <20163825@itla.edu.do>
2018-06-13 02:51:42 -0400

    Ha sido creado el componente Header

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 
 // Components
-import App from './global/App';
+import Header from './global/components/Header';
+import Content from './global/components/Content';
+import Footer from './global/components/Footer';
 
 // Styles
 import './index.css';
 import './global/styles/bootstrap/css/bootstrap.min.css';
 import './global/styles/bootstrap/js/fontawesome-all.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Header />, document.getElementById('header-root'));
+ReactDOM.render(<Content />, document.getElementById('content-root'));
+ReactDOM.render(<Footer />, document.getElementById('footer-root'));
